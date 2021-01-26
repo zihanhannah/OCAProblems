b1853e87d39caab1d1f316bf7c97efa4c8456008
zihan zhao <zihanhannah@gmail.com>
2021-01-26 14:19:08 -0600

    using instanceof operator to check if A is the subclass of B

--- src/B.java
+++ src/B.java
@@ -1,8 +1,28 @@
-class A{}
+interface Foo{}
+class A implements  Foo{}
 public class B extends A {
     public static void main(String[] args) {
         A myA = new B();
         m2(myA);
+        A a = new A();
+        B b = new B();
+        if(b instanceof A){
+            System.out.println("b is instanceof object A");
+        }
+        if(b instanceof  Foo){
+            System.out.println("b is instanceof interface object Foo, implemented indirectly");
+        }
+        if(a instanceof Foo){
+            System.out.println("a is instanceof interface object Foo");
+        }
+//        not true. check is-a
+        if(a instanceof B ){
+            System.out.println("a is instanceof B");
+        }
+//        null is not the instance of any object. always return false
+        if(!(null instanceof B)){
+            System.out.println("null is not instanceof any object, always return false");
+        }
     }
     public static void m2(A a){
         if(a instanceof B){
