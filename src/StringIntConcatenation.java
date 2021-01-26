4f485b29320d68c50dafc94926953d48e68bd63c
zihan zhao <zihanhannah@gmail.com>
2021-01-26 15:25:41 -0600

    checkout the string concatenation

---
+++ src/StringIntConcatenation.java
@@ -0,0 +1,10 @@
+public class StringIntConcatenation {
+    public static void main(String[] args) {
+        String s = "String";
+        int a = 5;
+        int b = 6;
+        System.out.println(s + a + b);
+        System.out.println(a + b + s);
+        System.out.println(a + b + s + a + b);
+    }
+}
